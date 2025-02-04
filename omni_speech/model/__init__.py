326882243f0d7001fc89d02bd40794be24d6edd2
riyadhrazzaq <riyadh.razzaq@gmail.com>
2025-02-05 00:15:30 +0100

    added llama-model import

--- omni_speech/model/__init__.py
+++ omni_speech/model/__init__.py
@@ -1,2 +1,6 @@
-from .language_model.omni_speech_llama import OmniSpeechLlamaForCausalLM, OmniSpeechConfig
+from .language_model.omni_speech_llama import (
+    OmniSpeechLlamaForCausalLM,
+    OmniSpeechConfig,
+    OmniSpeechLlamaModel,
+)
 from .language_model.omni_speech2s_llama import OmniSpeech2SLlamaForCausalLM
